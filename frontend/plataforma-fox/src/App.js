3189c16369b201142787a3d2328ffc458269aaf1
fontes99 <pvfontes1000@gmail.com>
2019-12-14 01:38:48 -0300

    cadastro com verificacao

--- frontend/plataforma-fox/src/App.js
+++ frontend/plataforma-fox/src/App.js
@@ -6,6 +6,7 @@ import './css/App.css'
 import Home from './Pages/Home'
 import Navbar from './Components/Navbar'
 import LoginCard from './Components/LoginCard'
+import RegisterCard from './Components/RegisterCard'
 
 function App() {
   return(
@@ -16,6 +17,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/login' component={LoginCard}/>
+          <Route exact path='/cadastro' component={RegisterCard}/>
         </Switch>
       </div>
     </Router>
